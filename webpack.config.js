fde2ffa2c22ab01a73708b6fb64f053023799a29
mostafaALBASH <mostafa_elbesh@outlook.com>
2026-01-30 17:19:44 +0100

    Configure Webpack to copy public/ files to dist/ root for Netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -24,6 +25,15 @@ module.exports = {
         removeComments: true,
         collapseWhitespace: true
       }
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: '',
+          noErrorOnMissing: true
+        }
+      ]
     })
   ],
   devServer: {
